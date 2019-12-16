96827e02c09cf5c56a5f03f1753023a86fca06be
ElektrischesSchaf <vertigo1958psycho1960@gmail.com>
2019-12-16 15:28:45 +0800

    modified

--- My_Code/task1/aicup_task1.py
+++ My_Code/task1/aicup_task1.py
@@ -639,14 +639,14 @@ class CNN(nn.Module):
 		
 		self.word_embeddings = nn.Embedding(vocab_size, embedding_length)
 		self.word_embeddings.weight = nn.Parameter(weights, requires_grad=False)
-		self.conv1 = nn.Conv2d(in_channels, out_channels, (kernel_heights[0], embedding_length), stride, padding)
-		self.conv2 = nn.Conv2d(in_channels, out_channels, (kernel_heights[1], embedding_length), stride, padding)
-		self.conv3 = nn.Conv2d(in_channels, out_channels, (kernel_heights[2], embedding_length), stride, padding)
+		self.conv1 = nn.Conv2d(16, in_channels, out_channels, (kernel_heights[0], embedding_length), stride, padding)
+		self.conv2 = nn.Conv2d(16, in_channels, out_channels, (kernel_heights[1], embedding_length), stride, padding)
+		self.conv3 = nn.Conv2d(16,in_channels, out_channels, (kernel_heights[2], embedding_length), stride, padding)
 		self.dropout = nn.Dropout(keep_probab)
 		#self.label = nn.Linear(len(kernel_heights)*out_channels, output_size)
 	
 	def conv_block(self, input, conv_layer):
-		conv_out = conv_layer(input)# conv_out.size() = (batch_size, out_channels, dim, 1)
+		conv_out = conv_layer(input) # conv_out.size() = (batch_size, out_channels, dim, 1)
 		activation = F.relu(conv_out.squeeze(3))# activation.size() = (batch_size, out_channels, dim1)
 		max_out = F.max_pool1d(activation, activation.size()[2]).squeeze(2)# maxpool_out.size() = (batch_size, out_channels)
 		
