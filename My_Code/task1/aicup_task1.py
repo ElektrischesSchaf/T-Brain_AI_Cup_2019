4c66694819e67e3a74376e53870394c2084fb6d5
ElektrischesSchaf <vertigo1958psycho1960@gmail.com>
2019-12-16 22:42:44 +0800

    modify

--- My_Code/task1/aicup_task1.py
+++ My_Code/task1/aicup_task1.py
@@ -833,7 +833,12 @@ def _run_epoch(epoch, mode):
     #for i, (x, y) in trange: # x = torch.LongTensor(batch_abstract), y = torch.FloatTensor(batch_label), sent_len = sent_len
     #train_iter, valid_iter, test_iter = data.BucketIterator.splits((trainset, validset, testset), batch_size=32, sort_key=lambda x: len(x.text), repeat=False, shuffle=True)
     print('\n train_loader\n')
-    train_loader = Data.DataLoader(dataset=trainset, batch_size=batch_size, shuffle=True)
+
+    df = pd.read_csv('trainset.csv')
+    target = pd.DataFrame(df['Onehot'])
+    train = data_utils.TensorDataset(torch.Tensor(np.array(df)), torch.Tensor(np.array(target)))
+    train_loader = data_utils.DataLoader(train, batch_size = 10, shuffle = True)
+
     for idx, batch in enumerate(train_loader):
         # Butters
         #print('In _run_epoch, i=', str(i), ' ', 'shape of x', x.shape, ' ', 'shape of y', y.shape, ' ', 'len of sent_len', len(sent_len), '\n')
