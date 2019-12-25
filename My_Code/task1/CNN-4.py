2307c2d55dbf12145299df59e3549075227c94d6
ElektrischesSchaf <vertigo1958psycho1960@gmail.com>
2019-12-25 18:35:21 +0800

    modify cnn-4

--- My_Code/task1/CNN-4.py
+++ My_Code/task1/CNN-4.py
@@ -424,6 +424,7 @@ def preprocess_sample(data, word_dict):
     ## convert the labels into one-hot encoding
     if 'Task 1' in data:
         processed['Label'] = [label_to_onehot(label) for label in data['Task 1'].split(' ')]
+        print('processed[Label]=',processed['Label'] )
         
     return processed
 
