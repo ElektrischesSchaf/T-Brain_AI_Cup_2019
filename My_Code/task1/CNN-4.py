b9de6a34e3927a4b17a17166b1ce1e1f37cbe41d
ElektrischesSchaf <vertigo1958psycho1960@gmail.com>
2019-12-26 11:46:48 +0800

    sperate_dict

--- My_Code/task1/CNN-4.py
+++ My_Code/task1/CNN-4.py
@@ -447,8 +447,8 @@ def sperate_dict( the_list ):
         if ('Abstract' in row)and('Label' in row): # suspecious
             for i in range(len(row['Abstract']) ):
                 new_dict={}
-                print(row, ' ')
-                print(row['Abstract'][i], '  ', row['Label'][i], '\n')
+                print('row= ', row, ' ')
+                print('in Abstact key= ', row['Abstract'][i], '\n in Label key= ', row['Label'][i], '\n')
                 new_dict['Abstract']=[ row['Abstract'][i] ]
                 new_dict['Label']=[ row['Label'][i] ]
                 output+=new_dict.get()
