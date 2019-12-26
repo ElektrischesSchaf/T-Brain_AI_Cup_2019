4723319396439120645844eec563f532e43a8e78
ElektrischesSchaf <vertigo1958psycho1960@gmail.com>
2019-12-26 11:50:11 +0800

    sperate_dict

--- My_Code/task1/CNN-4.py
+++ My_Code/task1/CNN-4.py
@@ -451,7 +451,7 @@ def sperate_dict( the_list ):
                 print('in Abstact key= ', row['Abstract'][i], '\n in Label key= ', row['Label'][i], '\n')
                 new_dict['Abstract']=[ row['Abstract'][i] ]
                 new_dict['Label']=[ row['Label'][i] ]
-                output+=new_dict.get()
+                output.append( new_dict.copy() )
     return output
 
 train_2=sperate_dict(train)
