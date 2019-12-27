f68ccd8df112e3dd1df8be0dd4f0ddbfc851a025
ElektrischesSchaf <vertigo1958psycho1960@gmail.com>
2019-12-27 12:13:51 +0800

    from torch.autograd import Variable

--- My_Code/task1/LSTM-1.py
+++ My_Code/task1/LSTM-1.py
@@ -14,6 +14,7 @@ import pickle, json, re, time
 import torch
 import torch.nn as nn
 import torch.nn.functional as F
+from torch.autograd import Variable
 from torch.utils.data import Dataset, DataLoader
 from torch.utils.tensorboard import SummaryWriter
 
