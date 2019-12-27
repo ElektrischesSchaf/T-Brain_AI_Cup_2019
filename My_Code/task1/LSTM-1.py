254fd9e363e701d00e839511630603a3ef2a0e04
ElektrischesSchaf <vertigo1958psycho1960@gmail.com>
2019-12-27 15:24:31 +0800

    batch  size

--- My_Code/task1/LSTM-1.py
+++ My_Code/task1/LSTM-1.py
@@ -606,8 +606,9 @@ class LSTMClassifier(nn.Module):
 		# input.size() = (num_sequences, batch_size, embedding_length)
 
 		if batch_size is None:
+			print('1 self.batch_size_LSTM= ', self.batch_size_LSTM, '\n')
 			self.batch_size_LSTM=10
-			print('self.batch_size_LSTM= ', self.batch_size_LSTM, '\n')
+			print('2 self.batch_size_LSTM= ', self.batch_size_LSTM, '\n')
 			h_0 = Variable(torch.zeros(1, self.batch_size_LSTM, self.hidden_size).cuda()) # Initial hidden state of the LSTM
 			c_0 = Variable(torch.zeros(1, self.batch_size_LSTM, self.hidden_size).cuda()) # Initial cell state of the LSTM
 		else:
