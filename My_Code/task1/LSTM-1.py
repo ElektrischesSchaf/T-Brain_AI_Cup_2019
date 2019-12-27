7b59374e266142b948fb049fb3726f1b1e76a47e
ElektrischesSchaf <vertigo1958psycho1960@gmail.com>
2019-12-27 15:52:11 +0800

    check

--- My_Code/task1/LSTM-1.py
+++ My_Code/task1/LSTM-1.py
@@ -548,79 +548,80 @@ testData = AbstractDataset(test, PAD_TOKEN, max_len = 64)
 
 
 class LSTMClassifier(nn.Module):
-	def __init__(self, batch_size, output_size, hidden_size, vocab_size, embedding_length, weights):
-		super(LSTMClassifier, self).__init__()
-		
-		"""
-		Arguments
-		---------
-		batch_size : Size of the batch which is same as the batch_size of the data returned by the TorchText BucketIterator
-		output_size : 2 = (pos, neg)
-		hidden_sie : Size of the hidden_state of the LSTM
-		vocab_size : Size of the vocabulary containing unique words
-		embedding_length : Embeddding dimension of GloVe word embeddings
-		weights : Pre-trained GloVe word_embeddings which we will use to create our word_embedding look-up table 
-		
-		"""
-		
-		self.batch_size_LSTM = batch_size
-		self.output_size = output_size
-		self.hidden_size = hidden_size
-		self.vocab_size = vocab_size
-		self.embedding_length = embedding_length
-		
-		self.word_embeddings = nn.Embedding(vocab_size, embedding_length)# Initializing the look-up table.
-		self.word_embeddings.weight = nn.Parameter(weights, requires_grad=False) # Assigning the look-up table to the pre-trained GloVe word embedding.
-		self.lstm = nn.LSTM(embedding_length, hidden_size)
-		self.label = nn.Linear(hidden_size, output_size)
-		
-	def forward(self, input_sentence, batch_size=None): # TODO must fix this	
-		""" 
-		Parameters
-		----------
-		input_sentence: input_sentence of shape = (batch_size, num_sequences)
-		batch_size : default = None. Used only for prediction on a single sentence after training (batch_size = 1)
-		
-		Returns
-		-------
-		Output of the linear layer containing logits for positive & negative class which receives its input as the final_hidden_state of the LSTM
-		final_output.shape = (batch_size, output_size)
-		
-		"""
-		
-		''' Here we will map all the indexes present in the input sequence to the corresponding word vector using our pre-trained word_embedddins.'''
-
-		# input_sentence.size() = (batch_size, 1, num of words)
-		#print('1 input_sentence.size()=', input_sentence.size(), '\n')
-
-		input = self.word_embeddings(input_sentence)
-		# embedded input of shape = (batch_size, 1, num of words,  embedding_length)		
-		#print('2 input.size()=', input.size(), '\n')
+    def __init__(self, batch_size, output_size, hidden_size, vocab_size, embedding_length, weights):
+        super(LSTMClassifier, self).__init__()
         
-		input=input.squeeze(1)
-		#print('3 input.size()=', input.size(), '\n')
-		#embedded input of shape = (batch_size, num of words,  embedding_length)
+        """
+        Arguments
+        ---------
+        batch_size : Size of the batch which is same as the batch_size of the data returned by the TorchText BucketIterator
+        output_size : 2 = (pos, neg)
+        hidden_sie : Size of the hidden_state of the LSTM
+        vocab_size : Size of the vocabulary containing unique words
+        embedding_length : Embeddding dimension of GloVe word embeddings
+        weights : Pre-trained GloVe word_embeddings which we will use to create our word_embedding look-up table 
         
-		input = input.permute(1, 0, 2)
-		#print('4 input.size()=', input.size(), '\n')
-		# input.size() = (num_sequences, batch_size, embedding_length)
+        """
+        
+        self.batch_size_LSTM = batch_size
+        self.output_size = output_size
+        self.hidden_size = hidden_size
+        self.vocab_size = vocab_size
+        self.embedding_length = embedding_length
+        
+        self.word_embeddings = nn.Embedding(vocab_size, embedding_length)# Initializing the look-up table.
+        self.word_embeddings.weight = nn.Parameter(weights, requires_grad=False) # Assigning the look-up table to the pre-trained GloVe word embedding.
+        self.lstm = nn.LSTM(embedding_length, hidden_size)
+        self.label = nn.Linear(hidden_size, output_size)
+        
+    def forward(self, input_sentence, batch_size=None): # TODO must fix this    
+        """ 
+        Parameters
+        ----------
+        input_sentence: input_sentence of shape = (batch_size, num_sequences)
+        batch_size : default = None. Used only for prediction on a single sentence after training (batch_size = 1)
+        
+        Returns
+        -------
+        Output of the linear layer containing logits for positive & negative class which receives its input as the final_hidden_state of the LSTM
+        final_output.shape = (batch_size, output_size)
+        
+        """
+        
+        ''' Here we will map all the indexes present in the input sequence to the corresponding word vector using our pre-trained word_embedddins.'''
 
-		if batch_size is None:
-			#print('1 self.batch_size_LSTM= ', self.batch_size_LSTM, '\n')
-			#self.batch_size_LSTM=10
-			#print('2 self.batch_size_LSTM= ', self.batch_size_LSTM, '\n')
-			h_0 = Variable(torch.zeros(1, self.batch_size_LSTM, self.hidden_size).cuda()) # Initial hidden state of the LSTM
-			c_0 = Variable(torch.zeros(1, self.batch_size_LSTM, self.hidden_size).cuda()) # Initial cell state of the LSTM
-		else:
-			h_0 = Variable(torch.zeros(1, batch_size, self.hidden_size).cuda())
-			c_0 = Variable(torch.zeros(1, batch_size, self.hidden_size).cuda())
-		output, (final_hidden_state, final_cell_state) = self.lstm(input, (h_0, c_0))
-		final_output = self.label(final_hidden_state[-1]) # final_hidden_state.size() = (1, batch_size, hidden_size) & final_output.size() = (batch_size, output_size)
+        # input_sentence.size() = (batch_size, 1, num of words)
+        #print('1 input_sentence.size()=', input_sentence.size(), '\n')
 
-		final_output=final_output.unsqueeze(1)
-		final_output=torch.sigmoid(final_output)
-		
-		return final_output
+        input = self.word_embeddings(input_sentence)
+        # embedded input of shape = (batch_size, 1, num of words,  embedding_length)        
+        #print('2 input.size()=', input.size(), '\n')
+        
+        input=input.squeeze(1)
+        #print('3 input.size()=', input.size(), '\n')
+        #embedded input of shape = (batch_size, num of words,  embedding_length)
+        
+        input = input.permute(1, 0, 2)
+        #print('4 input.size()=', input.size(), '\n')
+        # input.size() = (num_sequences, batch_size, embedding_length)
+
+        if batch_size is None:
+            #print('1 self.batch_size_LSTM= ', self.batch_size_LSTM, '\n')
+            #self.batch_size_LSTM=10
+            #print('2 self.batch_size_LSTM= ', self.batch_size_LSTM, '\n')
+            h_0 = Variable(torch.zeros(1, self.batch_size_LSTM, self.hidden_size).cuda()) # Initial hidden state of the LSTM
+            c_0 = Variable(torch.zeros(1, self.batch_size_LSTM, self.hidden_size).cuda()) # Initial cell state of the LSTM
+            print('size of h_0: ', h_0.size(), ', size of c_0: ', c_0.size(), '\n')
+        else:
+            h_0 = Variable(torch.zeros(1, batch_size, self.hidden_size).cuda())
+            c_0 = Variable(torch.zeros(1, batch_size, self.hidden_size).cuda())
+        output, (final_hidden_state, final_cell_state) = self.lstm(input, (h_0, c_0))
+        final_output = self.label(final_hidden_state[-1]) # final_hidden_state.size() = (1, batch_size, hidden_size) & final_output.size() = (batch_size, output_size)
+
+        final_output=final_output.unsqueeze(1)
+        final_output=torch.sigmoid(final_output)
+        
+        return final_output
 
 
 device = torch.device('cuda' if torch.cuda.is_available() else 'cpu')
